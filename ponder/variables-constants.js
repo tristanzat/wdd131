432c9152b2cd95a994de16e88e9f270a1484bede
Tristan Zatylny <tristan.zatylny@portnox.com>
2026-01-09 17:43:58 -0700

    add script in html and javscript file for ponder

---
+++ ponder/variables-constants.js
@@ -0,0 +1,32 @@
+const PI = 3.14;
+let radius = 3;
+
+let area = (radius ** 2) * PI;
+
+console.log("area of a circle of radius " + radius + ": " + area)
+
+radius = 20;
+area = (radius ** 2) * PI;
+
+console.log("area of a circle of radius " + radius + ": " + area)
+
+
+const one = 1;
+const two = '2';
+
+let result = one * two;
+console.log(result);
+
+result = one + Number(two);
+console.log(result);
+
+
+let course = "CSE131"; //global scope
+if (true) {
+    let student = "John";
+    console.log(course);  //works just fine, course is global
+    console.log(student); //works just fine, it's being accessed within the block
+}
+console.log(course); //works fine, course is global
+console.log(student); //does not work, can't access a block variable outside the block
+                    
