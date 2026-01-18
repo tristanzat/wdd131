4250389310a71065a76484b67314ae5a7219a97a
Tristan Zatylny <tristan.zatylny@portnox.com>
2026-01-17 22:46:21 -0700

    updated dark mode background color to be the correct shade (#333) and added a couple comments for better readability

--- mission/main.js
+++ mission/main.js
@@ -8,12 +8,16 @@ selectElem.addEventListener('change', changeTheme);
 function changeTheme() {
     let current = selectElem.value;
     if (current == 'dark') {
+        // Change BYU-I logo to be the white version
         logo.setAttribute('src', 'byui-logo-white.png');
+        // Change colors to dark mode; lighter text on a dark background.
         body.style.color = 'white';
-        body.style.backgroundColor = '#222';
+        body.style.backgroundColor = '#333';
         byui.style.color = '#8ebede';
     } else {
+        // Change BYU-I logo to be the blue version
         logo.setAttribute('src', 'byui-logo-blue.webp');
+        // Change colors to light mode; dark text on a light background.
         body.style.color = 'black';
         body.style.backgroundColor = 'white';
         byui.style.color = '#006eb6';
