fefad38c51c6826f3dc78cb5bf209b0048c9f3a3
Tristan Zatylny <tristan.zatylny@portnox.com>
2026-02-26 10:37:57 -0700

    fix typo

--- ponder/08-objects/courses.js
+++ ponder/08-objects/courses.js
@@ -20,7 +20,7 @@ console.log(aCourse.code);
 console.log(aCourse.name);
 
 document.querySelector("#courseName").textContent = aCourse.name;
-document.querySelector("#coursecode").textContent = aCourse.code;
+document.querySelector("#courseCode").textContent = aCourse.code;
 
 document.querySelector("img").setAttribute("src", aCourse.logo);
 document.querySelector("img").setAttribute("alt", aCourse.name);
